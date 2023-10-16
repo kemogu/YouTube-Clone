73811c9baddc2250cbb3b014d03ce8d2945d5544
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-16 19:19:32 +0300

    Header Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header.js";
+import Sidebar from "./components/sidebar/Sidebar.js";
+import HomeScreen from "./screens/homeScreen/HomeScreen.js";
+import { Container } from "react-bootstrap";
 
-function App() {
+import "./_app.scss";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="app__container border boder-info">
+        <Sidebar />
+        <Container fluid className="app__main border border-warning">
+          <HomeScreen />
+        </Container>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./_header.scss";
+
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <div className="border border-dark header">
+      <FaBars className="header__menu" size={26} />
+      <img
+        src="https://pngimg.com/uploads/youtube/youtube_PNG22.png"
+        alt=""
+        className="header__logo"
+      />
+      <form>
+        <input type="text" placeholder="Search" />
+        <button type="submit">
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
+      <dic className="header__icons">
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img
+          src="https://pngimg.com/uploads/man/man_PNG6513.png"
+          alt="avatar"
+        />
+      </dic>
+    </div>
+  );
+};
+
+export default Header;
